43f3e0de493fc3b232131d5d9273adf382ad117b
Domingo1987 <domingoperez1987@gmail.com>
2023-03-19 18:56:05 -0300

    c

--- Proyecto1/MiPrimerEjemplo.java
+++ Proyecto1/MiPrimerEjemplo.java
@@ -1,3 +1,10 @@
+/**
+ * @brief Clase que contiene el método main para imprimir un mensaje por pantalla.
+ * Esta clase contiene el método main que imprime por pantalla un mensaje de "Hola mundo!".
+ * @author Domingo Pérez
+ * @date 16/04/2023
+*/
+
 public class MiPrimerEjemplo {
    public static void main(String[] args) {
       System.out.println("Hola mundo!");
