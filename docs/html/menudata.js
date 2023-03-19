0692043cf83a1776722678ca0685a04c9aa88aa2
Domingo1987 <domingoperez1987@gmail.com>
2023-03-19 19:21:00 -0300

    doxygen menu

--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -23,4 +23,6 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Página principal",url:"index.html"}]}
+{text:"Página principal",url:"index.html"},
+{text:"Archivos",url:"files.html",children:[
+{text:"Lista de archivos",url:"files.html"}]}]}
