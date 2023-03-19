b4e1fc5586b8a9694cf2dea1e4be50bee0a2383f
Domingo1987 <domingoperez1987@gmail.com>
2023-03-19 19:37:49 -0300

    menu doxy

--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -24,5 +24,6 @@
 */
 var menudata={children:[
 {text:"Página principal",url:"index.html"},
+{text:"Páginas relacionadas",url:"pages.html"},
 {text:"Archivos",url:"files.html",children:[
 {text:"Lista de archivos",url:"files.html"}]}]}
--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,18 +1,21 @@
 var indexSectionsWithContent =
 {
-  0: "m",
-  1: "m"
+  0: "mr",
+  1: "mr",
+  2: "r"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "files"
+  1: "files",
+  2: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "Todo",
-  1: "Archivos"
+  1: "Archivos",
+  2: "Páginas"
 };
 
