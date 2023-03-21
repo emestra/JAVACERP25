9ca267b4023da369be0aa6926984d3c7b4b46854
Domingo1987 <domingoperez1987@gmail.com>
2023-03-21 09:37:12 -0300

    New Doc web

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['actionperformed_0',['actionPerformed',['../classPreguntasDelCurso_1_1AdminGUI.html#ab9729e1f32d18640b75729da5f24daa7',1,'PreguntasDelCurso.AdminGUI.actionPerformed()'],['../classPreguntasDelCurso_1_1InicioGUI.html#a9affc1f118b88f796825708d7614ef1d',1,'PreguntasDelCurso.InicioGUI.actionPerformed()']]],
+  ['actionperformed_0',['actionPerformed',['../classPreguntasDelCurso_1_1AdminGUI.html#ab9729e1f32d18640b75729da5f24daa7',1,'PreguntasDelCurso.AdminGUI.actionPerformed()'],['../classPreguntasDelCurso_1_1InicioGUI.html#a9affc1f118b88f796825708d7614ef1d',1,'PreguntasDelCurso.InicioGUI.actionPerformed()'],['../classPreguntasDelCurso_1_1PreguntaGUI.html#a34e71784fab7a72168c22d3d0d3424be',1,'PreguntasDelCurso.PreguntaGUI.actionPerformed()']]],
   ['addquestion_1',['addQuestion',['../classPreguntasDelCurso_1_1PreguntasList.html#abdc3aa40d085ce963337f27d9e4b753b',1,'PreguntasDelCurso::PreguntasList']]],
-  ['admingui_2',['AdminGUI',['../classPreguntasDelCurso_1_1AdminGUI.html#ae955263adbade57037de0eb5029c8ab2',1,'PreguntasDelCurso.AdminGUI.AdminGUI()'],['../classPreguntasDelCurso_1_1AdminGUI.html',1,'PreguntasDelCurso.AdminGUI']]],
+  ['admingui_2',['AdminGUI',['../classPreguntasDelCurso_1_1AdminGUI.html#a3243109796100941b0b3faa17f2fb9e0',1,'PreguntasDelCurso.AdminGUI.AdminGUI()'],['../classPreguntasDelCurso_1_1AdminGUI.html',1,'PreguntasDelCurso.AdminGUI']]],
   ['admingui_2ejava_3',['AdminGUI.java',['../AdminGUI_8java.html',1,'']]]
 ];
--- docs/html/search/functions_7.js
+++ docs/html/search/functions_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['pregunta_0',['Pregunta',['../classPreguntasDelCurso_1_1Pregunta.html#acdbf335937d6db92722d59b62e89f4c3',1,'PreguntasDelCurso::Pregunta']]],
-  ['preguntagui_1',['PreguntaGUI',['../classPreguntasDelCurso_1_1PreguntaGUI.html#a3245190779e0f375041100b6a0bc6b37',1,'PreguntasDelCurso::PreguntaGUI']]],
+  ['preguntagui_1',['PreguntaGUI',['../classPreguntasDelCurso_1_1PreguntaGUI.html#af7c6bed2d7ba230b6bfa54253d31f0b6',1,'PreguntasDelCurso::PreguntaGUI']]],
   ['preguntaslist_2',['PreguntasList',['../classPreguntasDelCurso_1_1PreguntasList.html#a17b327893d6a1d9c364b7e2990e307a6',1,'PreguntasDelCurso::PreguntasList']]]
 ];
